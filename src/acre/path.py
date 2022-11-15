6d7595bf64b8e78725d2f555f322fab66581157e
Claudio Klingler <ck@realtime-projects.com>
2022-11-15 12:25:45 +0100

    acre/path.py: fix directory determination.

--- src/acre/path.py
+++ src/acre/path.py
@@ -5,7 +5,7 @@ class AcrePath:
 
     @staticmethod
     def base():
-        return os.path.dirname(os.path.dirname(__file__))
+        return os.path.dirname(__file__)
 
     @staticmethod
     def steps():
