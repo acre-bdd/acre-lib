b682e4d6311bc3e760c4b0a20ed6bd59f8fcab6f
Claudio Klingler <ck@realtime-projects.com>
2022-11-15 12:07:25 +0100

    Make AcrePath methods static.

--- src/acre/tools/path.py
+++ src/acre/tools/path.py
@@ -3,8 +3,10 @@ import os
 
 class AcrePath:
 
+    @staticmethod
     def base():
         return os.path.dirname(os.path.dirname(__file__))
 
+    @staticmethod
     def steps():
         return os.path.join(AcrePath.base(), "steps")
