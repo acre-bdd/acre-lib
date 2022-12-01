6d3d44f0c40bbcad27446b682e2511121a86586b
Claudio Klingler <ck@realtime-projects.com>
2022-12-01 21:14:04 +0100

    optimize and fix logging.

--- src/acre/hooks/logging.py
+++ src/acre/hooks/logging.py
@@ -38,42 +38,42 @@ def setup_logging(features, marker):
     logfh.setLevel(log.DEBUG)
     logging.getLogger().addHandler(logfh)
     log.warning(f"{artifacts}/{trid}")
-    log.trace(f"testrun {marker} {trid} started")
+    log.trace(f"TESTRUN|started|{marker}|{trid}")
 
 
 @before.each_feature
 def before_feature(feature):
     world.tid = tid.tid_from_tags(feature.tags)
-    log.debug(f"started: {feature} [{world.tid}]")
+    log.debug(f"FEATURE|started|{feature} [{world.tid}]")
 
 
 @after.each_feature
 def after_feature(feature):
-    log.debug(f"{feature.state}: {feature}")
+    log.debug(f"FEATURE|{feature.state}|{feature}")
 
 
 @before.each_scenario
 def before_scenario(scenario):
-    log.debug(f"started: Scenario: {scenario.sentence}")
+    log.debug(f"SCENARIO|started|{scenario.sentence}")
 
 
-@after.each_feature
+@after.each_scenario
 def after_scenario(scenario):
-    log.debug(f"{scenario.state}: Scenario: {scenario.sentence}")
+    log.debug(f"SCENARIO|{scenario.state}|{scenario.sentence}")
 
 
 @before.each_step
 def before_step(step):
-    log.debug(f"started: Step: {step.sentence}")
+    log.debug(f"STEP|started|{step.sentence}")
 
 
 @after.each_step
 def after_step(step):
-    log.debug(f"{step.state}: Step: {step.sentence}")
+    log.debug(f"STEP|{step.state}|{step.sentence}")
 
 
 @after.all
 def finish_logging(features, marker):
     global logfile
-    log.trace(f"testrun {marker} {trid} finished")
+    log.trace(f"TESTRUN|finished|{marker}|{trid}")
     log.highlight(f"logs written to: {logfile}")
