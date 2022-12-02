5642fc5a84e2ce46871e8502ef135ad9e8d4c262
Claudio Klingler <ck@realtime-projects.com>
2022-12-02 02:38:46 +0100

    setup monitor logging.

--- src/acre/hooks/logging.py
+++ src/acre/hooks/logging.py
@@ -1,5 +1,7 @@
+import time
 import os
 import logging
+from termcolor import colored
 from acre import log, userdata, settings
 
 from radish import before, after, world
@@ -14,12 +16,14 @@ levelmap = {
 
 trid = None
 logfile = None
+logmon = None
 
 
 @before.all(order=1)
 def setup_logging(features, marker):
     global trid
     global logfile
+    global logmon
     level = 'info'
     if userdata.loglevel:
         level = userdata.loglevel.lower()
@@ -36,44 +40,61 @@ def setup_logging(features, marker):
     logfh = logging.FileHandler(os.path.join(artifacts, f"{trid}.log"))
     logfh.setFormatter(logging.Formatter("%(asctime)s|%(levelname)s|%(message)s"))
     logfh.setLevel(log.DEBUG)
+    logmon = logging.getLogger().getChild("logmon")
+    logmon.propagate = False
+    logmonh = logging.FileHandler(os.path.join(artifacts, "/tmp/monitor2.log"))
+    logmonh.setLevel(log.INFO)
+    logmonh.setFormatter(logging.Formatter("%(asctime)s %(message)s"))
+    logmon.addHandler(logmonh)
     logging.getLogger().addHandler(logfh)
     log.warning(f"{artifacts}/{trid}")
-    log.trace(f"TESTRUN|started|{marker}|{trid}")
+    log.trace(f"TESTRUN {marker}|{trid}")
+    logmon.info(colored(f"TESTRUN {marker}|{trid}", "magenta"))
 
 
 @before.each_feature
 def before_feature(feature):
-    world.tid = tid.tid_from_tags(feature.tags)
-    log.debug(f"FEATURE|started|{feature} [{world.tid}]")
+    world.tid = tid.tid_from_tags(feature)
+    log.debug(f"FEATURE: {feature} [{world.tid}]")
+    logmon.info(colored(f"FEATURE: {feature} [{world.tid}]", "white", attrs=['bold']))
 
 
 @after.each_feature
 def after_feature(feature):
-    log.debug(f"FEATURE|{feature.state}|{feature}")
+    log.debug(f"FEATURE: {feature.state}|{feature}")
+    logmon.info(colored(f"FEATURE: {feature.state}|{feature}", "white", attrs=['bold']))
 
 
 @before.each_scenario
 def before_scenario(scenario):
-    log.debug(f"SCENARIO|started|{scenario.sentence}")
+    log.debug(f"    SCENARIO: {scenario.sentence}")
+    logmon.info(colored(f"    SCENARIO: {scenario.sentence}", "white", attrs=['bold']))
 
 
 @after.each_scenario
 def after_scenario(scenario):
-    log.debug(f"SCENARIO|{scenario.state}|{scenario.sentence}")
+    log.debug(f"    SCENARIO {scenario.state}: {scenario.sentence}")
+    logmon.info(colored(f"    SCENARIO {scenario.state}: {scenario.sentence}", "white", attrs=['bold']))
 
 
 @before.each_step
 def before_step(step):
-    log.debug(f"STEP|started|{step.sentence}")
+    if userdata.delay:
+        time.sleep(int(userdata.delay))
+    log.debug(f"        STEP: {step.sentence}")
+    logmon.info(colored(f"        {step.sentence}", "yellow"))
 
 
 @after.each_step
 def after_step(step):
-    log.debug(f"STEP|{step.state}|{step.sentence}")
+    log.debug(f"        STEP {step.state}:  {step.sentence}")
+    color = "green" if "passed" in step.state else "red"
+    logmon.info(colored(f"        {step.sentence}", color))
 
 
 @after.all
 def finish_logging(features, marker):
     global logfile
     log.trace(f"TESTRUN|finished|{marker}|{trid}")
+    logmon.info(f"TESTRUN|finished|{marker}|{trid}")
     log.highlight(f"logs written to: {logfile}")
